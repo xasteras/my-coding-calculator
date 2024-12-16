51a60ca587067f9b68b55edbbf471ff50731ff0b
xasteras <xasteras@gmail.com>
2024-12-16 20:56:38 +0200

    div method created

--- src/gr/aueb/cf/calc/Main.java
+++ src/gr/aueb/cf/calc/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Whatever");
+
     } public static int add (int a, int b) {
         return a + b;
     }
+
+    public static int div (int a, int b) {return a / b;}
 }
