37d816b6e6c644e1c1c8b16a47416ac6c1e67b3e
xasteras <xasteras@gmail.com>
2024-12-16 20:33:36 +0200

    project created

---
+++ src/gr/aueb/cf/calc/Main.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.calc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Whatever");
+    } public static int add (int a, int b) {
+        return a + b;
+    }
+}
